4a22ed9c96e0b7fd855ace4afc995e4cb1467590
Rushi2008-new <rushimaheshwari184@gmail.com>
2026-08-08 09:59:26 +0530

    Todo using CONTEXT

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -7,7 +7,7 @@ import TodoItem from './components/TodoItem'
 function App() {
   const [todos, setTodos] = useState([])
 
-  //Add Functionality
+  // Add todo Functionality
   const addTodo = (todo) => {
     setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev])
   }
